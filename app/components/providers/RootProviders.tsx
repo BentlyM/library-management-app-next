f4e0c389f0c08fbbc28c2cd2dbf5aa595eb8fd04
BentlyM <metayerbently@gmail.com>
2024-10-13 11:54:56 -0400

    added dev tools for my sanity

--- app/components/providers/RootProviders.tsx
+++ app/components/providers/RootProviders.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function RootProviders({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient({}));
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
         {children}
     </QueryClientProvider>
   );
