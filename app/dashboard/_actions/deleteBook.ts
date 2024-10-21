84ee5c19b17f520e847f77c1061402227bb48c70
BentlyM <metayerbently@gmail.com>
2024-10-20 21:39:33 -0400

    "Wrap delete operations in try-catch block and add error handling"

--- app/dashboard/_actions/deleteBook.ts
+++ app/dashboard/_actions/deleteBook.ts
@@ -5,17 +5,22 @@ import prisma from '@/app/lib/prisma';
 export async function DeleteBook(id: string) {
   if (!id) return { success: false, message: 'Book cannot be identified' };
 
-  await prisma.readingProgress.deleteMany({
-    where: { bookId: id },
-  });
+  try {
+    await prisma.readingProgress.deleteMany({
+      where: { bookId: id },
+    });
 
-  const deletedBook = await prisma.book.delete({
-    where: {
-      id,
-    },
-  });
+    const deletedBook = await prisma.book.delete({
+      where: {
+        id,
+      },
+    });
 
-  if (!deletedBook) return { success: false, message: 'Book Not Found!' };
+    if (!deletedBook) return { success: false, message: 'Book Not Found!' };
 
-  return { success: true, data: deletedBook };
+    return { success: true, data: deletedBook };
+  } catch (error) {
+    console.error('Error deleting book', error);
+    return { success: false, message: 'unable to delete book.' };
+  }
 }
