29ece835764536e253405e0dc8eab0a14508abcd
BentlyM <metayerbently@gmail.com>
2024-10-20 21:30:17 -0400

    fixed book not being able to get deleted when progress updated or created.

--- app/dashboard/_actions/deleteBook.ts
+++ app/dashboard/_actions/deleteBook.ts
@@ -5,6 +5,10 @@ import prisma from '@/app/lib/prisma';
 export async function DeleteBook(id: string) {
   if (!id) return { success: false, message: 'Book cannot be identified' };
 
+  await prisma.readingProgress.deleteMany({
+    where: { bookId: id },
+  });
+
   const deletedBook = await prisma.book.delete({
     where: {
       id,
